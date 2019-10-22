60a884bd4a8e7c01e6b704778b4a02e7d1da5167
Nick Richmond <nwrichmond@turnt.tech>
2019-10-22 17:01:15 -0400

    Wrap app in an Apollo provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,27 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from 'react';
+import PropTypes from 'prop-types';
+import fetch from 'isomorphic-fetch';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 
-// You can delete this file if you're not using it
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  // Use FakeQL's GraphQL API mocking as a service
+  uri: 'https://fakeql.com/graphql/b392a1321ec1a1d6f3894a2f827b3064',
+  fetch,
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+);
+
+wrapRootElement.propTypes = {
+  element: PropTypes.element.isRequired,
+};
