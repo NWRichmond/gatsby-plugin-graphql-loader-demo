780c50056637f2b278e562f33521b9d44828c6b1
Nick Richmond <nwrichmond@turnt.tech>
2019-10-22 17:22:18 -0400

    Explain and demonstrate the query flow

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,73 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { UserInfo } from '../components/user-info';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const queryString = `query getUserComments($firstname: String!) {
+  users(where: { firstname_eq: $firstname }) {
+    firstname
+    comments {
+      text
+    }
+  }
+}`;
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [user, setUser] = useState('Sophia');
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Welcome</h1>
+      <p>
+        This page fetches fake blog post data from a GraphQL API on the client.
+        A GraphQL query is loaded directly from a <code>.gql</code> file using{' '}
+        <a
+          href="https://github.com/NWRichmond/gatsby-plugin-graphql-loader"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          gatsby-plugin-graphql-loader
+        </a>{' '}
+        and is then passed to Apollo Client.
+      </p>
+      <p>
+        <a
+          href="https://fireql.dev/?url=https://fakeql.com/graphql/b392a1321ec1a1d6f3894a2f827b3064"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here
+        </a>{' '}
+        to explore the fake dataset (blog posts with authors and comments) and
+        experiment with GraphQL queries.
+      </p>
+      <p>
+        The query contained in the <code>.gql</code> file is as follows:
+      </p>
+      <pre style={{ maxWidth: `500px` }}>
+        <code>{queryString}</code>
+      </pre>
+      <p>
+        ...which is saved as a <code>.gql</code> file and imported into a React
+        component which uses Apollo Client to query the GraphQL endpoint.
+      </p>
+      <h3>Select A User:</h3>
+      <div style={{ margin: `1rem auto` }}>
+        <select
+          onChange={event => {
+            setUser(event.target.value);
+          }}
+        >
+          <option value="Sophia">Sophia</option>
+          <option value="Hulda">Hulda</option>
+          <option value="Lucy">Lucy</option>
+          <option value="Brett">Brett</option>
+        </select>
+      </div>
+      <h2>Query Result:</h2>
+      <UserInfo firstname={user} />
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
